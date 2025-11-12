8cba9826aa7da597368c4165607d6382f41b4b53
Talha-2023 <syedtalhaahmed123786@gmail.com>
2025-11-12 13:52:20 +0530

    small chagnes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import AppDownloadSection from "../components/HomeComponents/AppDownloadSection"
 import HeroSection from "../components/HomeComponents/HeroSection";
 import HowItWorks from "../components/HomeComponents/HowItWorks";
 import ImpressiveCollection from "../components/HomeComponents/ImpressiveCollection";
+import RentByBodyType from "../components/HomeComponents/RentByBodyType";
+import RentByBrands from "../components/HomeComponents/RentByBrand";
+import RentByProviders from "../components/HomeComponents/RentByProviders";
 import SearchBar from "../components/HomeComponents/SearchBar";
 import ServicesSection from "../components/HomeComponents/ServicesSection";
 import TestimonialSection from "../components/HomeComponents/TestimonialSection";
@@ -10,6 +13,9 @@ const Home = () => (
   <>
     <HeroSection />
     <SearchBar />
+    <RentByProviders />
+    <RentByBrands />
+    <RentByBodyType />
     <ImpressiveCollection />
     <HowItWorks />
     <ServicesSection />
